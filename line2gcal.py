a060723bd23b92d5d8b27a75e7d9367e7ea91aa2
Weicheng Pan <weichengpan@gmail.com>
2016-09-05 10:32:16 +0800

    Separate OAuth steps into two commands

--- line2gcal.py
+++ line2gcal.py
@@ -1,11 +1,12 @@
 from __future__ import print_function
+
+import traceback
 from collections import namedtuple
 import httplib2
 import os
 from apiclient import discovery
-import oauth2client
+import oauth2client.tools
 from oauth2client import client
-from oauth2client import tools
 import parsedatetime as pdt
 import shlex
 import boto3
@@ -13,7 +14,6 @@ from datetime import datetime as dt
 from time import mktime
 import datetime
 from linebot.client import LineBotClient
-import argparse
 import json
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
@@ -58,53 +58,87 @@ class DynamodbStorage(oauth2client.client.Storage):
         self.id = id
 
 
-def get_credentials():
-    store = DynamodbStorage("sigmapan")
+def get_credentials(mid):
+    store = DynamodbStorage(mid)
     credentials = store.get()
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
-        flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else:  # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
+        flow.redirect_uri = client.OOB_CALLBACK_URN
+        authorize_url = flow.step1_get_authorize_url()
+        msg = "Please authorize application: " + authorize_url + "\n" + "/gcal-auth [code]"
+        raise Exception(msg)
     return credentials
 
 
-def parse_args(inputString):
-    argv = shlex.split(inputString)
-    # parser = argparse.ArgumentParser()
-    # argv = parser.parse_args(args=arr)
-    if argv[0] != "/gcal":
-        exit(0)
-    Args = namedtuple("Args", "title when duration who location description")
-    args = Args(duration=60, title=argv[1], when=argv[2], description="", location="", who="")
+def lambda_handler(event, context):
+    try:
+        msg = process_input(event['result'][0]['content']['from'], event['result'][0]['content']['text'])
+        send_msg(event['result'][0]['content']['from'], msg)
+    except Exception as err:
+        send_msg(event['result'][0]['content']['from'], err.message)
+    return "Done"
+
+
+def process_gcal(mid, argv):
+    GcalArgs = namedtuple("Args", "title when duration who location description calendar")
+    args = GcalArgs(duration=60, title=argv[1], when=argv[2], description="", location="", who="", calendar=None)
     # TODO: fill the args
 
-    return args
-
-
-def lambda_handler(event, context):
-    process_command(event['result'][0]['content']['text'])
-    send_msg(event['result'][0]['content']['from'], "Done!!")
-    return "ok"
-
-
-def process_command(command):
-    args = parse_args(command)
-
-    set_timezone()
-    credentials = get_credentials()
+    credentials = get_credentials(mid)
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
 
-    calendar_id = get_calendar_by_name(service, "family")
+    calendar_id = get_calendar_by_name(service, args.calendar)
     return create_event(service, calendar_id, args)
 
 
+def process_gcal_auth(mid, argv):
+    flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+    flow.redirect_uri = client.OOB_CALLBACK_URN
+    credentials = flow.step2_exchange(code=argv[1])
+    store = DynamodbStorage(mid)
+    store.put(credentials)
+    return "Link successful"
+
+
+def process_help():
+    return """
+/gcal [title] [when]
+/gcal-auth [code]
+/gcal-default [calendar_name]
+/help
+"""
+
+
+def process_gcal_default(mid, argv):
+    pass
+
+
+def process_input(mid, input_string):
+    set_timezone()
+    argv = shlex.split(input_string)
+    if argv[0][0] != '/':
+        raise Exception("Not a command")
+    cmd = argv[0][1:]
+    if cmd == "gcal":
+        return process_gcal(mid, argv)
+    elif cmd == "gcal-auth":
+        return process_gcal_auth(mid, argv)
+    elif cmd == "gcal-default":
+        return process_gcal_default(mid, argv)
+    elif cmd == "help":
+        return process_help()
+    else:
+        raise Exception("Unsupported command - " + cmd)
+
+
 def main():
-    print(process_command("/gcal 'Hello World' +1d"))
+    try:
+        print(process_input("sigmapan", "/gcal 'Hello World' +1d"))
+        # print(process_input("sigmapan", "/gcal-auth 4/6Krpo2_4GB-30reEFhpq5robnymXEDdRX9kXzl7S2OY"))
+    except Exception as err:
+        print(err)
 
 
 def create_event(service, calendarId, args):
@@ -131,12 +165,16 @@ def create_event(service, calendarId, args):
 
 
 def get_calendar_by_name(service, name):
+    if not name:
+        return "primary"
     calendar_list = service.calendarList().list().execute().get('items', [])
-    calendar_id = ""
+    calendar_id = None
     for calendar in calendar_list:
         if calendar['summary'] == name:
             calendar_id = calendar['id']
             break
+    if not calendar_id:
+        raise Exception("Calendar " + name + " not found")
     return calendar_id
 
 
